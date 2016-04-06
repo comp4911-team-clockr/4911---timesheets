2432e4d514eabc3a66fdc09e35307ee70b5708ee
don <dli9431@gmail.com>
2016-04-05 17:22:29 -0700

    edited statusreport manager

--- COMP4911Project/JavaSource/comp4911/managers/StatusReportManager.java
+++ COMP4911Project/JavaSource/comp4911/managers/StatusReportManager.java
@@ -16,45 +16,61 @@ import comp4911.models.WorkPackage;
 @Stateless
 public class StatusReportManager implements Serializable {
 	/**
-	 * 
+	 * RNG serialVersionUID
 	 */
 	private static final long serialVersionUID = 5463145269486045946L;
 	@PersistenceContext(unitName="COMP4911ClockrProjectDatabase") EntityManager em;
 	
+	// db query to find status report through PK
 	public StatusReport find(String id) {
 		return em.find(StatusReport.class, id);
 	}
 	
+	// db query to create new status report
 	public void persist(StatusReport sr) {
 		em.persist(sr);
 	}
 	
+	// db query to update existing status report
 	public void merge(StatusReport sr) {
 		em.merge(sr);
 	}
 
-//	public StatusReport findByUserId(String id) {
-//		TypedQuery<StatusReport> query = em.createQuery("SELECT s FROM StatusReport s WHERE s. = :statusReportId ", StatusReport.class);
-//		query.setParameter("statusReportId", id);
-//		java.util.List<StatusReport> results = query.getResultList(); 
-//		StatusReport sr = results.get(0);
-//		return sr;
-//	}
+	// db query to find status report based on responsibility engineer id
+	public StatusReport findByREngId(String id) {
+		TypedQuery<StatusReport> query = em.createQuery("SELECT s FROM StatusReport s "+
+				"WHERE s.RespEngId=" + "'" + id + "'", StatusReport.class);
+		java.util.List<StatusReport> results = query.getResultList(); 
+		StatusReport sr = results.get(0);
+		return sr;
+	}
 	
+	// db query to find all reports in a WP
 	public java.util.List<StatusReport> getAllByWorkPackage (int id) {
         TypedQuery<StatusReport> query = em.createQuery("SELECT s FROM StatusReport s "+
-        		"WHERE s.isActive IS TRUE AND s.wpId=" + id,
+        		"WHERE s.wpId=" + id,
                 StatusReport.class); 
         java.util.List<StatusReport> reports = query.getResultList();
         return reports;
 	}
 	
+	// db query to find all status reports in db
 	public java.util.List<StatusReport> getAll() {
-        TypedQuery<StatusReport> query = em.createQuery("SELECT s FROM StatusReport s "+
-        		"WHERE s.isActive IS TRUE",
+        TypedQuery<StatusReport> query = em.createQuery("SELECT s FROM StatusReport s ",
                 StatusReport.class); 
         java.util.List<StatusReport> reports = query.getResultList();
         return reports;
+	}
+	
+	/* db query to find status report by certain variance greater than decimal input
+	ex: .5 = 50% variance
+	*/
+	public java.util.List<StatusReport> getByCostVariance (double cost)
+	{
+		TypedQuery<StatusReport> query = em.createQuery("Select s from StatusReport s "+
+				"WHERE s.costVar > " + cost, StatusReport.class);
+		java.util.List<StatusReport> reports = query.getResultList();
+		return reports;
 	}
 	
 }
