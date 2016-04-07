86a64738e2cf0b6ef67577511cc367d58e3c1fd3
Carlo Conti <carlolorenzo_conti@yahoo.ca>
2016-04-06 17:22:09 -0700

    Updated clockr.sql to have more seeded values for employees, credential, timesheet, timesheet row tables

--- clockr.sql
+++ clockr.sql
@@ -22,11 +22,16 @@ CREATE TABLE Employees(	EmpNum int,
 						PayRateId TINYTEXT
 						);
                                                 
-INSERT INTO Employees VALUES (1, "Bob", "Smith", "000001", 2, 10, 0, "2010-10-10", "aa@aa.aa", "P1");
-INSERT INTO Employees VALUES (2, "Jane", "Doe", "000002", 2, 10, 0, "1995-01-01", "bb@bb.bb", "P5");
-INSERT INTO Employees VALUES (3, "Cisco", "Ramon", "000003", 2, 10, 0, "2010-01-01", "cc@cc.cc", "P3");
-INSERT INTO Employees VALUES (4, "Barry", "Allen", "000004", 2, 10, 0, "2011-01-01", "dd@dd.dd", "P3");
-
+INSERT INTO Employees VALUES (1, "Bob", "Smith", "000001", 3, 10, 0, "2010-10-10", "aa@aa.aa", "P1");
+INSERT INTO Employees VALUES (2, "Jane", "Doe", "000002", 3, 10, 0, "1995-01-01", "bb@bb.bb", "P5");
+INSERT INTO Employees VALUES (3, "Cisco", "Ramon", "000003", 4, 10, 0, "2010-01-01", "cc@cc.cc", "P3");
+INSERT INTO Employees VALUES (4, "Barry", "Allen", "000004", 3, 10, 0, "2011-01-01", "dd@dd.dd", "P3");
+INSERT INTO Employees VALUES (5, "Bruce", "Wayne", "000005", 3, 5, 0, "2011-10-07", "ee@ee.ee", "P4");
+INSERT INTO Employees VALUES (6, "Clark", "Kent", "000006", 4, 10, 0, "2011-11-01", "ff@ff.ff", "P3");
+INSERT INTO Employees VALUES (7, "Sally", "Sue", "000007", 3, 10, 0, "2011-12-01", "gg@hh.hh", "P2");
+INSERT INTO Employees VALUES (8, "Emily", "Doe", "000008", 3, 10, 0, "2012-01-01", "hh@hh.hh", "DS");
+INSERT INTO Employees VALUES (9, "Wally", "West", "000009", 4, 10, 0, "2012-02-05", "ii@ii.ii", "SS");
+INSERT INTO Employees VALUES (10, "Jackee", "Ma", "000010", 4, 10, 0, "1994-10-07", "jj@jj.jj", "P3");
 DROP TABLE IF EXISTS Credentials;
 CREATE TABLE Credentials( UserId TINYTEXT, 
 						  Password TINYTEXT, 
@@ -44,25 +49,60 @@ INSERT INTO Credentials VALUES ("000001", "password", 001, "Employee", "aa@aa.aa
                                 "",
                                 "1504678463");
                                 
-INSERT INTO Credentials VALUES ("000002", "iamPM", 002, "ProjectManager", "bb@bb.bb" , 
+INSERT INTO Credentials VALUES ("000002", "password", 002, "ProjectManager", "bb@bb.bb" , 
                                 "", 
                                 "",
                                 "",
                                 "-1889844622");
 
-INSERT INTO Credentials VALUES ("000003", "iamgod", 003, "SystemAdmin", "cc@cc.cc" ,
+INSERT INTO Credentials VALUES ("000003", "password", 003, "HumanResource", "cc@cc.cc" ,
                                 "", 
                                 "",
                                 "",
                                 "-58714991");
 
-INSERT INTO Credentials VALUES ("000004", "f3@rMe", 004, "ResponsibilityEngineer", "dd@dd.dd" , 
+INSERT INTO Credentials VALUES ("000004", "password", 003, "HumanResource", "dd@dd.dd" , 
                                 "", 
                                 "",
                                 "",
                                 "128438349");
-
                                 
+INSERT INTO Credentials VALUES ("000005", "password", 002, "ProjectManager", "ee@ee.ee" , 
+                                "", 
+                                "",
+                                "",
+                                "");
+
+INSERT INTO Credentials VALUES ("000006", "password", 001, "Employee", "ff@ff.ff" , 
+                                "", 
+                                "",
+                                "",
+                                "");
+
+INSERT INTO Credentials VALUES ("000007", "password", 001, "Employee", "gg@gg.gg" , 
+                                "", 
+                                "",
+                                "",
+                                "");
+                               
+INSERT INTO Credentials VALUES ("000008", "password", 002, "ProjectManager", "hh@hh.hh" , 
+                                "", 
+                                "",
+                                "",
+                                "");
+                                
+INSERT INTO Credentials VALUES ("000009", "password", 001, "Employee", "ii@ii.ii" , 
+                                "", 
+                                "",
+                                "",
+                                "");
+                                
+INSERT INTO Credentials VALUES ("000010", "password", 001, "Employee", "jj@jj.jj" , 
+                                "", 
+                                "",
+                                "",
+                                "");
+                                                                
 DROP TABLE IF EXISTS PayRate;
 CREATE TABLE PayRate( PayRateId TINYTEXT,
 					  CostinMD double,
@@ -99,7 +139,16 @@ CREATE TABLE TimeSheet( TimesheetId TINYTEXT,
                         IsActive BOOL);
 						
 INSERT INTO TimeSheet VALUES("2|1", 2, 25, "2016-02-01", 160, 0, 0, 32, 32, 32, 32, 32, 0, "", TRUE, FALSE, TRUE);	
-                        
+INSERT INTO TimeSheet VALUES("1|1", 1, 25, "2016-02-01", 160, 0, 0, 32, 32, 32, 32, 32, 0, "", FALSE, FALSE, TRUE);
+INSERT INTO TimeSheet VALUES("3|1", 3, 25, "2016-02-01", 100, 0, 0, 20, 20, 20, 20, 20, 0, "", FALSE, FALSE, TRUE);
+INSERT INTO TimeSheet VALUES("4|1", 4, 26, "2016-02-08", 130, 0, 0, 25, 25, 25, 25, 30, 0, "", TRUE, FALSE, TRUE);
+INSERT INTO TimeSheet VALUES("5|1", 5, 26, "2016-02-08", 160, 0, 0, 32, 32, 32, 32, 32, 0, "", FALSE, FALSE, TRUE);
+INSERT INTO TimeSheet VALUES("6|1", 6, 27, "2016-02-15", 160, 0, 0, 32, 32, 32, 32, 32, 0, "", TRUE, FALSE, TRUE);	
+INSERT INTO TimeSheet VALUES("7|1", 7, 27, "2016-02-15", 150, 0, 0, 25, 25, 25, 25, 50, 0, "", FALSE, FALSE, TRUE);
+INSERT INTO TimeSheet VALUES("8|1", 8, 27, "2016-02-15", 140, 0, 0, 30, 20, 30, 20, 40, 0, "", TRUE, FALSE, TRUE);
+INSERT INTO TimeSheet VALUES("9|1", 9, 27, "2016-02-15", 130, 0, 0, 25, 25, 25, 25, 30, 0, "", FALSE, FALSE, TRUE);
+INSERT INTO TimeSheet VALUES("10|1", 10, 27, "2016-02-15", 120, 0, 0, 30, 30, 30, 10, 20, 0, "", TRUE, FALSE, TRUE);				                        
+
 DROP TABLE IF EXISTS TimeSheetRow;
 CREATE TABLE TimeSheetRow( TimeSheetRowId TINYTEXT,
                            TimeSheetId TINYTEXT,
@@ -117,9 +166,21 @@ CREATE TABLE TimeSheetRow( TimeSheetRowId TINYTEXT,
                            FlextimeHrs double,
                            Notes TINYTEXT);
 
-INSERT INTO TimeSheetRow VALUES("2|1|1", "2|1", 10, 1, 40, 0, 0, 8, 8, 8, 8, 8, 0, 0, "much codin");	
-INSERT INTO TimeSheetRow VALUES("2|1|2", "2|1", 10, 1, 44, 0, 0, 12, 8, 8, 8, 8, 0, 4, "such hax");
-INSERT INTO TimeSheetRow VALUES("2|1|3", "2|1", 10, 1, 40, 0, 0, 0, 8, 8, 8, 8, 1, 0, "very db");	
+INSERT INTO TimeSheetRow VALUES("1|1|1", "1|1", 1, 1, 40, 0, 0, 8, 8, 8, 8, 8, 0, 0, "much codin");	
+INSERT INTO TimeSheetRow VALUES("1|1|2", "1|1", 1, 1, 44, 0, 0, 12, 8, 8, 8, 8, 0, 0, "such hax");
+INSERT INTO TimeSheetRow VALUES("1|1|3", "1|1", 1, 1, 32, 0, 0, 0, 8, 8, 8, 8, 1, 0, "very db");	
+INSERT INTO TimeSheetRow VALUES("2|1|1", "2|1", 2, 1, 40, 0, 0, 8, 8, 8, 8, 8, 0, 0, "i be chillin");  
+INSERT INTO TimeSheetRow VALUES("2|1|2", "2|1", 2, 1, 44, 0, 0, 12, 8, 8, 8, 8, 0, 0, "like a pro");
+INSERT INTO TimeSheetRow VALUES("2|1|3", "2|1", 2, 1, 40, 0, 0, 1, 8, 8, 8, 8, 1, 7, "droppin beats");
+INSERT INTO TimeSheetRow VALUES("2|1|4", "2|1", 2, 1, 37, 0, 0, 5, 8, 8, 8, 8, 0, 0, "in slow mo"); 
+INSERT INTO TimeSheetRow VALUES("3|1|1", "3|1", 1, 1, 41, 0, 0, 9, 8, 8, 8, 8, 0, 0, "my individuality");
+INSERT INTO TimeSheetRow VALUES("4|1|1", "4|1", 1, 1, 41, 0, 0, 8, 8, 8, 8, 9, 0, 0, "gets less and less");
+INSERT INTO TimeSheetRow VALUES("5|1|1", "5|1", 2, 1, 40, 0, 0, 8, 7, 8, 8, 9, 0, 0, "further down");
+INSERT INTO TimeSheetRow VALUES("6|1|1", "6|1", 1, 1, 39, 0, 0, 8, 7, 7, 8, 9, 0, 0, "this rabbit hole");
+INSERT INTO TimeSheetRow VALUES("7|1|1", "7|1", 2, 1, 42, 0, 0, 10, 7, 8, 8, 9, 0, 0, "i am the dark"); 
+INSERT INTO TimeSheetRow VALUES("8|1|1", "8|1", 1, 1, 36, 0, 0, 8, 7, 7, 5, 9, 0, 0, "i am batman");
+INSERT INTO TimeSheetRow VALUES("9|1|1", "9|1", 2, 1, 40, 0, 0, 8, 7, 8, 8, 9, 0, 0, "i really dig this website");
+INSERT INTO TimeSheetRow VALUES("10|1|1", "10|1", 2, 1, 40, 0, 0, 8, 7, 8, 8, 9, 0, 0, "hey supervisor guy, im diggin your sweater last week");
 						 
 DROP TABLE IF EXISTS Project;
 CREATE TABLE Project( ProjectId int NOT NULL, 
